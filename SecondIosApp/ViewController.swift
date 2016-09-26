46cd598db0a8cbc9fb4f8b1127bb1c5dfb0d928d
GearShift24 <jwri6847@740s-pl-369.local>
2016-09-26 08:55:23 -0600

    tried to all ui outlet to view controller. added button and label

--- SecondIosApp/ViewController.swift
+++ SecondIosApp/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var firstButton: UIButton!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func clickedButton(sender: UIButton)
+    {
+        textLabel.text = "You clicked me!"
+        textLabel.textColor = UIColor.cyanColor()
+        
+    }
 
 }
 
